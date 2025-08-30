825a439f4539bba727ecfc9cbaa88d4947a8dfe3
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2025-08-30 17:32:58 +0000

    Replaced the root / handler  with homePage

--- main.go
+++ main.go
@@ -1,39 +1,44 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
+    // Render the home html page from static folder
+    http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
+    // Render the course html page
+    http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
-	http.ServeFile(w, r, "static/about.html")
+    // Render the about html page
+    http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
-	http.ServeFile(w, r, "static/contact.html")
+    // Render the contact html page
+    http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
+    // Root (/) should serve the homepage instead of just a text message
+    http.HandleFunc("/", homePage)
 
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
+    http.HandleFunc("/home", homePage)
+    http.HandleFunc("/courses", coursePage)
+    http.HandleFunc("/about", aboutPage)
+    http.HandleFunc("/contact", contactPage)
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    fmt.Println("🚀 Server running on port 8081...")
+    err := http.ListenAndServe(":8081", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
+
